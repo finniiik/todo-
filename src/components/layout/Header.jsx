07b3cb38eb1445b77fd1c5445e7f410166d486fa
finn <finniiik>
2026-06-30 00:42:57 +0300

    buttom

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -17,11 +17,6 @@ export const Header = ({ darkMode, setDarkMode, sidebarOpen, setSidebarOpen }) =
       </div>
       <div className="settings">
         <ul>
-          <li className="settings__add">
-            <button aria-label="Быстрое добавление задачи" type="button">
-              +
-            </button>
-          </li>
           <li className="settings__darkmode">
             <button
               aria-label="Тёмная тема"
